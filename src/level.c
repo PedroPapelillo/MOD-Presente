0ad8f2848b6b103f6050582325b4c4c82b4c4d33
PedroPapelillo <59874747+PedroPapelillo@users.noreply.github.com>
2020-05-18 19:41:50 -0400

    Juego

---
+++ src/level.c
@@ -0,0 +1,93 @@
+#include "level.h"
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <assert.h>
+
+level *level_new(int size_x, int size_y){
+    // Ask memory for the level struct
+    level *lvl = malloc(sizeof(level));
+
+    // Set the size
+    lvl->size_x = size_x;
+    lvl->size_y = size_y;
+
+    // Ask memory for the cell* array
+    lvl->cells  = malloc(lvl->size_y * sizeof(char *));
+
+    // Ask memory for each row
+    for(int y=0;y<lvl->size_y;y++){
+        lvl->cells[y] = malloc(lvl->size_x * sizeof(char));
+    }
+
+    // Initialize level
+    for(int y=0;y<lvl->size_y;y++){
+        for(int x=0;x<lvl->size_x;x++){
+            lvl->cells[y][x] = '.';
+        }
+    }
+
+    // Retreieve level's pointer
+    return lvl;
+}
+
+int level_get(const level *lvl, int x, int y){
+    // For cells outside the matrix:
+    if(x<0) return '#';
+    if(y<0) return '#';
+    if(x>=lvl->size_x) return '#';
+    if(y>=lvl->size_y) return '#';
+    // Regular cells
+    return lvl->cells[y][x];
+}
+
+void level_fill_random(level *lvl, int wall_size){
+    // Check that the wall_size is smaller than the level
+    assert(wall_size<lvl->size_x);
+    assert(wall_size<lvl->size_y);
+
+    // Compute a good number of walls so that up to 1/10 of the level is filled
+    int n_walls = lvl->size_x*lvl->size_y/wall_size/10;
+//    int n_walls = 0;
+
+    // Add each of the walls
+    for(int i=0;i<n_walls;i++){
+        int vert = rand()%2; // vertical or horizontal? 1 = vert, 0 = hor
+        //La pared siempre se ubicará entre 0 en x o en y el tamaño del nivel en x
+        //o en y (si es horizontal o vertical respectivamente) menos el tamaño de
+        //la pared, luego, para "renderizarla", se llenaran tantas celdas en x o en
+        //y (nuevamente, si es horizontal o vertical respectivamente) como el numero
+        //del tamaño de la pared
+
+        if(vert){
+            // Initial wall position
+            int wx = rand()%(lvl->size_x);
+            int wy = rand()%(lvl->size_y-wall_size);
+
+            // Iterate to place wall vertically; 
+            for(int k=0;k<wall_size;k++){
+                lvl->cells[wy+k][wx] = '#';
+            }
+        }else{
+            // Initial wall position
+            int wx = rand()%(lvl->size_x-wall_size);
+            int wy = rand()%(lvl->size_y);
+            // Iterate to place wall horizontally
+            for(int k=0;k<wall_size;k++){
+                lvl->cells[wy][wx+k] = '#';
+            }
+        }
+    }
+}
+
+// Free level memory
+void level_free(level *lvl){
+    // Free each row first
+    for(int y=0;y<lvl->size_y;y++){
+        free(lvl->cells[y]);
+    }
+    // Free char* array
+    free(lvl->cells);
+    // Free the level
+    free(lvl);
+}
---
+++ src/main.c
@@ -0,0 +1,73 @@
+#include <raylib.h>
+#include <math.h>
+#include <stdio.h>
+
+#include "level.h"
+#include "draw.h"
+#include "state.h"
+
+int main(int argc, char const *argv[]){
+
+    // Initialization
+    const int screen_width = 800;
+    const int screen_height = 600;
+
+    InitWindow(screen_width,screen_height,"Presente, Fangshi MOD");
+    SetTargetFPS(60);
+    int frames = 0;
+
+    // Initialize level and fill randomly
+    level *lvl = level_new(50,40);
+    level_fill_random(lvl,6);
+
+    // Initialize state (and add enemies)
+    state *sta = state_new();
+    state_populate_random(lvl,sta,50);
+
+    // Main loop
+    while(!WindowShouldClose()){
+        // Update input depending if keys are pressed or not
+        sta->button_state[0] = IsKeyDown(KEY_D);
+        sta->button_state[1] = IsKeyDown(KEY_W);
+        sta->button_state[2] = IsKeyDown(KEY_A);
+        sta->button_state[3] = IsKeyDown(KEY_S);
+        sta->button_state[4] = IsMouseButtonDown(MOUSE_LEFT_BUTTON);
+        // Compute the angle the player is looking at from the mouse's position relative to the screen's center
+        float mouse_y = GetMouseY()-GetScreenHeight()/2;
+        float mouse_x = GetMouseX()-GetScreenWidth()/2;
+        sta->aim_angle = atan2(-mouse_y,mouse_x);
+
+        // Update the state
+        state_update(lvl,sta);
+
+        // Drawing
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            draw_state(lvl, sta);
+
+            DrawText("Presente profe!",20,20,20,LIGHTGRAY);
+
+            if(sta->n_enemies != 0)
+            {
+                DrawText(FormatText("Tiempo: %03i segundos", frames/60),20,40,20,RED);
+                DrawText(FormatText("Enemigos restantes: %03i", sta->n_enemies),20,60,20,RED);
+                frames+=1;
+            }else
+            {
+                DrawText(FormatText("You win! \nTiempo: %03i segundos", frames/60),100,150,60,RED);
+            }
+
+        EndDrawing();
+    }
+
+    // Close window
+    CloseWindow();
+
+    // Free memory
+    state_free(sta);
+    level_free(lvl);
+
+    return 0;
+}
